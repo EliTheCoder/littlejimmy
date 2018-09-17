324a6e3e26d48d39888d116dfc2128d44bead53b
elithecoder <ex.games.org@gmail.com>
2018-09-17 09:41:45 -0500

    added Super Mario Bros

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -48,6 +48,15 @@ void operatorControl() {
 			flyWheelActive = true;
 		}
 
+		if (joystickGetDigital(1, 7, JOY_RIGHT)) {
+			speakerPlayRtttl("Super Mario:d=4,o=5,b=100:16e6,16e6,32p,8e6,16c6,8e6,8g6,8p,8g,8p,"
+   "8c6,16p,8g,16p,8e,16p,8a,8b,16a#,8a,16g.,16e6,16g6,8a6,16f6,8g6,8e6,16c6,16d6,8b,16p,"
+   "8c6,16p,8g,16p,8e,16p,8a,8b,16a#,8a,16g.,16e6,16g6,8a6,16f6,8g6,8e6,16c6,16d6,8b,16p,"
+   "8p,16g6,16f#6,16f6,16d#6,16p,16e6,16p,16g#,16a,16c6,16p,16a,16c6,16d6,"
+   "8p,16g6,16f#6,16f6,16d#6,16p,16e6,16p,16c7,16p,16c7,16c7,8p.,"
+   "8p,16g6,16f#6,16f6,16d#6,16p,16e6,16p,16g#,16a,16c6,16p,16a,16c6,16d6," "8p,16d#6,8p,16d6,8p,16c6");
+		}
+
 		if (intakeActive) {
 			intakeSet(127);
 		} else {
