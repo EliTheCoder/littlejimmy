02f7480a45dc250d0e41b165dbefee515f5417e6
elithecoder <ex.games.org@gmail.com>
2018-09-28 09:42:52 -0500

    fixed fightsong

--- src/auto.c
+++ src/auto.c
@@ -31,5 +31,5 @@ void autonomous() {
   //speakerPlayRtttl("Unfortunately: d=8,o=4,b=120: c, c, e, g, a, g, e, f, p, f, p, e, d, e, d, c, c, c, e, g, a, g, e, 2a3, 2c");
   delay(2000);
   //speakerPlayRtttl("DropofWater: d=8,o=4,b=128: c, e, e, e, e, e, d, c, d, 4c, 4p, 2c5, 4p, b, a, g, a, g, e, d, e, 2p, p, c, e, e, e, e, e, d, c, d, 4c");
-  speakerPlayRtttl("FightSong: d=4,o=4,b=140: a, c4, b, a, g, e, c, d, 8e, 8g, 8p, 8f, e, d, c, 16g, 16g, 16g, 16p, 16g, 16g, 16g, 8e, 8f, 8g, 2e, d, e, 8f, 8f, 8p, 8e, 8f, p, 8f, 8f, 8p, 8f, e, f, 8g, 8g, 8p, 8f#, 8g, p, a, c4, b, a, g, e, c, d, 8e, 8g, 8p, 8f, e, d, c");
+  speakerPlayRtttl("FightSong: d=4,o=4,b=140: a, c5, b, a, g, e, c, d, 8e, 8g, 8p, 8f, e, d, c, 16g, 16g, 16g, 16p, 16g, 16g, 16g, 8e, 8f, 8g, 2e, d, e, 8f, 8f, 8p., 8e, 8f, p, 8f, 8f, 8p, 8f, e, f, 8g, 8g, 8p., 8f#, 8g, p, a, c5, b, a, g, e, c, d, 8e, 8g, 8p, 8f, e, d, c");
 }
