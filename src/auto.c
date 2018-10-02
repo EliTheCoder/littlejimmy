21111577149a0476874d68ac73cc3739e16fb4f1
elithecoder <ex.games.org@gmail.com>
2018-10-02 10:01:57 -0500

    fixed fight song a little bit

--- src/auto.c
+++ src/auto.c
@@ -31,6 +31,6 @@ void autonomous() {
   //speakerPlayRtttl("Unfortunately: d=8,o=4,b=120: c, c, e, g, a, g, e, f, p, f, p, e, d, e, d, c, c, c, e, g, a, g, e, 2a3, 2c");
   delay(2000);
   //speakerPlayRtttl("DropofWater: d=8,o=4,b=128: c, e, e, e, e, e, d, c, d, 4c, 4p, 2c5, 4p, b, a, g, a, g, e, d, e, 2p, p, c, e, e, e, e, e, d, c, d, 4c");
-  speakerPlayRtttl("FightSong: d=4,o=4,b=140: a, c5, b, a, g, e, c, d, 8e, 8g, 8p, 8f, e, d, 8c, 16g, 16g, 16g, 16p, 16g, 16g, 16g, 8e, 8f, 8g, 2e, d, e, 8f, 8f, 8p., 8e, 8f, p., 8f, 8f, 8p, 8f, e, f, 8g, 8g, 8p., 8f#, 8g, p., a, c5, b, a, g, e, c, d, 8e, 8g, 8p, 8f, e, d, 8c, 16g, 16g, 16g, 16p, 8g, c5");
+  speakerPlayRtttl("FightSong: d=4,o=4,b=140: a, c5, b, a, g, e, c, d, 8e, 8g, 8p, 8f, e, d, 8c, 16g, 16g, 16g, 16p, 16g, 16g, 16g, 8e, 8f, 8g, 2e, d, e, 8f, 8f, 8p, 8e, 8f, p., 8f, 8f, 8p, 8f, e, f, 8g, 8g, 8p, 8f#, 8g, p., a, c5, b, a, g, e, c, d, 8e, 8g, 8p, 8f, e, d, 8c, 16g, 16g, 16g, 16p, 8g, c5");
   speakerPlayRtttl("Fortnite: d=16,o=5,b=92: 4f3, f, g#, a#, 8a#., 8g#., p, 8p, 4f3, f, g#, a#, 8a#, 8g#, 8f, d#, 8f, 8p, a#, g#, f, d#, 2f");
 }
