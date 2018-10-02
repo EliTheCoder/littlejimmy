efc669f4b145419c85f441c4a156180f5ae0cdba
elithecoder <ex.games.org@gmail.com>
2018-10-02 09:32:42 -0500

    fixed default dance song

--- src/auto.c
+++ src/auto.c
@@ -32,5 +32,5 @@ void autonomous() {
   delay(2000);
   //speakerPlayRtttl("DropofWater: d=8,o=4,b=128: c, e, e, e, e, e, d, c, d, 4c, 4p, 2c5, 4p, b, a, g, a, g, e, d, e, 2p, p, c, e, e, e, e, e, d, c, d, 4c");
   speakerPlayRtttl("FightSong: d=4,o=4,b=140: a, c5, b, a, g, e, c, d, 8e, 8g, 8p, 8f, e, d, 8c, 16g, 16g, 16g, 16p, 16g, 16g, 16g, 8e, 8f, 8g, 2e, d, e, 8f, 8f, 8p., 8e, 8f, p., 8f, 8f, 8p, 8f, e, f, 8g, 8g, 8p., 8f#, 8g, p., a, c5, b, a, g, e, c, d, 8e, 8g, 8p, 8f, e, d, 8c, 16g, 16g, 16g, 16p, 8g, c5");
-  speakerPlayRtttl("Fortnite: d=16,o=5,b=92: 4f, f, g#, a6#, 8a#6., 8g#., p, 8p, 4f, f, g#, a#6, 8a#6, 8g#, 8f, d#6, 8f, 8p, a#6, g#, f, d#6, 2f");
+  speakerPlayRtttl("Fortnite: d=16,o=5,b=92: 4f3, f, g#, a#, 8a#., 8g#., p, 8p, 4f3, f, g#, a#, 8a#, 8g#, 8f, d#, 8f, 8p, a#, g#, f, d#, 2f");
 }
