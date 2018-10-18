94fc87e6df9ff55b67bc86b38e1fef76d7f6d2a8
AlexCheese <42589632+AlexCheese@users.noreply.github.com>
2018-10-18 16:04:40 -0500

    Reverted properly this time
    please work maybe

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -1,48 +1,68 @@
 #include "main.h"
 
-int lMotor1 = 3;
-int lMotor2 = 4;
-int rMotor1 = 2;
+bool turboMode = false;
+
+int lMotor1 = 1;
+int lMotor2 = 2;
+int lMotor3 = 3;
+
+int rMotor1 = 4;
 int rMotor2 = 5;
-int intake1 = 6;
-int conveyor1 = 1;
-int flyWheel1 = 7;
-int flyWheel2 = 8;
+int rMotor3 = 6;
+
+int intake1 = 7;
+
+int conveyor1 = 8;
+
+int flyWheel1 = 9;
+int flyWheel2 = 10;
 
 void lDriveSet(int control) {
 	motorSet(lMotor1, -control);
 	motorSet(lMotor2, -control);
+	motorSet(lMotor3, -control);
 }
 void rDriveSet(int control) {
 	motorSet(rMotor1, -control);
 	motorSet(rMotor2, -control);
+	motorSet(rMotor3, -control);
 }
 void flyWheelSet(int control) {
 	motorSet(flyWheel1, control);
 	motorSet(flyWheel2, control);
 }
+
 void intakeSet(int control) {
 	motorSet(intake1, control);
 }
+
 void conveyorSet(int control) {
 	motorSet(conveyor1, control);
 }
 
-
-
-
-
 void operatorControl() {
 	while (true) {
-		if (joystickGetAnalog(1, 3) > 5 || joystickGetAnalog(1, 3) < -5)
-			lDriveSet(joystickGetAnalog(1,3)^3/25000);
-		else
-			lDriveSet(0);
+		if (!turboMode) {
+			if (joystickGetAnalog(1, 3) > 5 || joystickGetAnalog(1, 3) < -5)
+				lDriveSet(joystickGetAnalog(1,3)^3/25000);
+			else
+				lDriveSet(0);
 
-		if (joystickGetAnalog(1, 2) > 5 || joystickGetAnalog(1, 2) < -5)
-			rDriveSet(joystickGetAnalog(1,2)^3/25000);
-		else
-			rDriveSet(0);
+			if (joystickGetAnalog(1, 2) > 5 || joystickGetAnalog(1, 2) < -5)
+				rDriveSet(joystickGetAnalog(1,2)^3/25000);
+			else
+				rDriveSet(0);
+		} else {
+			if (joystickGetAnalog(1, 3) > 5 || joystickGetAnalog(1, 3) < -5)
+				lDriveSet(joystickGetAnalog(1,3));
+			else
+				lDriveSet(0);
+
+			if (joystickGetAnalog(1, 2) > 5 || joystickGetAnalog(1, 2) < -5)
+				rDriveSet(joystickGetAnalog(1,2));
+			else
+				rDriveSet(0);
+		}
 
 		if (joystickGetDigital(1, 5, JOY_DOWN)) {
 			intakeSet(127);
@@ -59,11 +79,13 @@ void operatorControl() {
 		if (joystickGetDigital(1, 6, JOY_UP)) {
 			flyWheelSet(127);
 		} else {
-			if (joystickGetDigital(1, 6, JOY_DOWN)) {
-				flyWheelSet(-127);
-			} else {
-				flyWheelSet(0);
-			}
+			flyWheelSet(0);
+		}
+
+		if (joystickGetDigital(1, 6, JOY_DOWN)) {
+			turboMode = true;
+		} else {
+			turboMode = false;
 		}
 
 		delay(25);
